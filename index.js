726b553f554d7ebd0dd95a2ebc2871460c8dfcc9
Jeff-tech67 <geofreytech@yahoo.com>
2025-12-07 22:54:28 +0300

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
 
 
 
-module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
-};
+// module.exports = {
+//   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+// };
+
+
+// Step 1: Define the outer function
+function createLoginTracker(userInfo) {
+    // initialize attempt counter
+    let attemptCount = 0;
+
+    // Step 2: Define and return the inner arrow function
+    const attemptLogin = (passwordAttempt) => {
+        // increment attempts every time function is called
+        attemptCount++;
+
+        // If attempts exceed 3 ,lock account
+        if (attemptCount > 3) {
+            return "Account locked due to too many failed login attempts";
+        }
+
+        // Check if entered password matches
+        if (passwordAttempt === userInfo.password) {
+            return "Login successful";
+        } else {
+            return `Attempt ${attemptCount}: Login failed`;
+        }
+    };
+
+    // return the inner function (closure)
+    return attemptLogin;
+}
+
+const Jeff = createLoginTracker({
+    username: "Jeff",
+    password: "12345678"
+    
+  });
+
+  console.log(Jeff("helloworld"));
+  console.log(Jeff("kjbilhb"))
+  console.log(Jeff("12345678"))
+  console.log(Jeff("kyky"))
